e721ef003f0ed3ba45b77f3e75f8afb79b0db1d4
rafasilva9537 <rafasilva9537@gmail.com>
2024-04-29 06:40:31 -0300

    conectando banco de dados e adição das primeiras rotas

--- WebApp/routes/materia.js
+++ WebApp/routes/materia.js
@@ -1,14 +1,36 @@
 import express from 'express';
 import path from 'node:path';
+import pool from '../services/database.js'
 import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); //pega o nome do diretório
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
     //__dirname é o caminho absoluto
-    res.sendFile(path.join(__dirname, '../public', 'materiaTeste.html'));
+    //res.sendFile(path.join(__dirname, '../public', 'materiaTeste.html'));
+    
+    try{
+        const resultado = await pool.query('SELECT * FROM Topico');
+        const obj = resultado.rows;
+        console.log(obj)
+        res.status(200).json(resultado.rows);
+    }
+    catch(error){
+        return res.status(500).json({error: error.message});
+    }
 });
+
+
+
+router.put('/', async(req, res) => {
+    try{
+
+    }
+    catch(error){
+        return res.status(500).json({error: error.message})
+    }
+})
 
 export default router;
--- WebApp/services/database.js
+++ WebApp/services/database.js
@@ -1,10 +1,17 @@
-//const { Pool } = require('pg')
-import { Pool } from 'pg'
+import pg from 'pg'
+import dotenv from "dotenv"
+dotenv.config()
 
+const { Pool } = pg
+
+//coloquem suas variáveis locais do postgre no arquivo .env
+//arquivo ignorado pelo github
 const pool = new Pool({
-    host: '',
-    user: '',
-    password: '',
-    port: '',
-    database: ''
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE
 })
+
+export default pool;
